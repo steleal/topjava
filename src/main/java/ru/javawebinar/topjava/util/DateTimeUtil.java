b4de426234387f84364f8744c312c56c05c2ff0d
Alexey Shtele <aleksey.shtele@rt.ru>
2023-02-13 14:09:39 +0400

    HW02: Review - change return type of isBetweenHalfOpen method to generics

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -7,12 +7,8 @@ import java.time.format.DateTimeFormatter;
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
-    }
-
-    public static boolean isBetweenHalfOpen(LocalDateTime ldt, LocalDateTime startDateTime, LocalDateTime endDateTime) {
-        return ldt.compareTo(startDateTime) >= 0 && ldt.compareTo(endDateTime) < 0;
+    public static <T extends Comparable<T>> boolean isBetweenHalfOpen(T value, T start, T end) {
+        return value.compareTo(start) >= 0 && value.compareTo(end) < 0;
     }
 
     public static String toString(LocalDateTime ldt) {
