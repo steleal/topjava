326ac4e396461d65cf7faac9b3980e6e9132f534
Alexey Shtele <aleksey.shtele@rt.ru>
2023-03-27 17:11:50 +0400

    HW07: Add RootControllerTest.getMeals

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,14 +2,24 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static ru.javawebinar.topjava.UserTestData.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_MATCHER;
+import static ru.javawebinar.topjava.UserTestData.admin;
+import static ru.javawebinar.topjava.UserTestData.guest;
+import static ru.javawebinar.topjava.UserTestData.user;
 
 class RootControllerTest extends AbstractControllerTest {
 
@@ -28,5 +38,17 @@ class RootControllerTest extends AbstractControllerTest {
                             }
                         }
                 ));
+    }
+
+    @Test
+    void getMeals() throws Exception {
+        SecurityUtil.setAuthUserId(ADMIN_ID);
+        List<MealTo> expectedAdminMeals = MealsUtil.getTos(MealTestData.adminMeals, admin.getCaloriesPerDay());
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", expectedAdminMeals)               );
     }
 }
