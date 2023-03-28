58895896794d73fe67158de6aa3bce1e14df75d3
Alexey Shtele <aleksey.shtele@rt.ru>
2023-03-28 20:05:23 +0400

    HW07: Review - fix MealTo and RootControllerTest

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -49,6 +49,6 @@ class RootControllerTest extends AbstractControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("meals"))
                 .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
-                .andExpect(model().attribute("meals", expectedAdminMeals)               );
+                .andExpect(model().attribute("meals", expectedAdminMeals));
     }
 }
